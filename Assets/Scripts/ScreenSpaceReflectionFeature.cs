e4fed3228d601494747d0b2ae634202b860ac0df
kyrosz7u <kyrosz7u@gmail.com>
2023-07-19 17:17:46 +0800

    去除冗余代码
    (cherry picked from commit 9cff0ccf99b7e5090f2b31179834c633f0a3b51e)

--- Assets/Scripts/ScreenSpaceReflectionFeature.cs
+++ Assets/Scripts/ScreenSpaceReflectionFeature.cs
@@ -1,45 +1,37 @@
 ﻿using UnityEngine;
-using UnityEngine.Experimental.Rendering.Universal;
-using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
-using UnityEngine.Serialization;
 
-namespace RendererFeature
+public class ScreenSpaceReflectionFeature: ScriptableRendererFeature
 {
-    public class ScreenSpaceReflectionFeature: ScriptableRendererFeature
+    [System.Serializable]
+    public class ScreenSpaceReflectionSettings
     {
-        [System.Serializable]
-        public class ScreenSpaceReflectionSettings
-        {
-            public RenderPassEvent Event = RenderPassEvent.AfterRenderingOpaques;
+        public RenderPassEvent Event = RenderPassEvent.AfterRenderingOpaques;
 
-            public float MaxSteps = 32;
-            public float StepSize = 0.5f;
-            public float MaxDistance = 10;
-            [Range(0, 1)]public float Thickness = 0.1f;
-            [Range(0, 1)]public float ResolutionScale = 0.5f;
-            [Range(-0.5f, 0.5f)]public float ReflectionBlurSpread = 0;
-            [Range(0, 1)]public float LuminanceCloseOpThreshold = 0.5f;
-        }
+        public float MaxSteps = 32;
+        public float StepSize = 0.5f;
+        public float MaxDistance = 10;
+        [Range(0, 1)]public float Thickness = 0.1f;
+        [Range(0, 1)]public float ResolutionScale = 0.5f;
+        [Range(-0.5f, 0.5f)]public float ReflectionBlurSpread = 0;
+        [Range(0, 1)]public float LuminanceCloseOpThreshold = 0.5f;
+    }
 
-        public ScreenSpaceReflectionSettings settings = new ScreenSpaceReflectionSettings();
-        public ScreenSpaceReflectionPass m_ScreenSpaceReflectionPass;
+    public ScreenSpaceReflectionSettings settings = new ScreenSpaceReflectionSettings();
+    private ScreenSpaceReflectionPass m_ScreenSpaceReflectionPass;
 
-        private RenderTargetHandle m_ScreenSpaceReflectionTexture;
+    private RenderTargetHandle m_ScreenSpaceReflectionTexture;
         
-        
-        public override void Create()
-        {
+    public override void Create()
+    {
             
-        }
+    }
 
-        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
-        {
-            m_ScreenSpaceReflectionPass = new ScreenSpaceReflectionPass();
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        m_ScreenSpaceReflectionPass = new ScreenSpaceReflectionPass();
+        m_ScreenSpaceReflectionPass.Setup(settings.Event, settings, (UniversalRenderer)renderer);
             
-            m_ScreenSpaceReflectionPass.Setup(settings.Event, settings, (UniversalRenderer)renderer);
-            
-            renderer.EnqueuePass(m_ScreenSpaceReflectionPass);
-        }
+        renderer.EnqueuePass(m_ScreenSpaceReflectionPass);
     }
 }
