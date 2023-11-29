9888ce0ca8f250f457fb9d34e0b59504ad824e02
kyrosz7u <kyrosz7u@gmail.com>
2023-11-29 13:09:31 +0800

    hiz feature

---
+++ Assets/Scripts/HiZGeneraterFeature.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace UnityTemplateProjects
+{
+    public class HiZGeneraterFeature: ScriptableRendererFeature
+    {
+        public RenderTexture hizMap;
+        private HiZGeneraterPass m_HiZGeneraterPass;
+        public override void Create()
+        {
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            hizMap = new RenderTexture(renderer.cameraDepthTarget.descriptor);
+            m_HiZGeneraterPass = new HiZGeneraterPass();
+            
+            m_HiZGeneraterPass.Setup(RenderPassEvent.AfterRenderingOpaques, renderingData.cameraData.cameraColorTarget);
+        }
+    }
+}
---
+++ Assets/Scripts/HiZGeneraterPass.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace UnityTemplateProjects
+{
+    public class HiZGeneraterPass: ScriptableRenderPass
+    {
+        public RenderTexture hizMap;
+        public void Setup(RenderPassEvent renderPassEvent, RenderTexture hizMap)
+        {
+            this.renderPassEvent = renderPassEvent;
+            this.hizMap = hizMap;
+        }
+        
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            var cmd = CommandBufferPool.Get("HiZGeneraterPass");
+            cmd.Clear();
+            cmd.GenerateMips(renderingData.cameraData.cameraColorTarget);
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+    }
+}
