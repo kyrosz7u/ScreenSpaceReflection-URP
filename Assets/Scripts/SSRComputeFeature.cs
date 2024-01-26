44a67293062c6c8d3325d9a6953f3c5679b74f02
kyrosz7u <kyrosz7u@gmail.com>
2024-01-26 18:35:20 +0800

    ComputerShader版SSR

---
+++ Assets/Scripts/SSRComputeFeature.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+
+public class SSRComputeFeature: ScriptableRendererFeature
+{
+    [System.Serializable]
+    public class SSRComputeFeatureSettings
+    {
+        public RenderPassEvent Event = RenderPassEvent.AfterRenderingOpaques;
+
+        public float MaxSteps = 32;
+        public float MaxDistance = 100;
+        [Range(0, 20)]public float Thickness = 0.1f;
+        [Range(0, 1)]public float ReflectionStride = 0.5f;
+        [Range(0, 3)]public float ReflectionJitter = 1.0f;
+        [Range(-0.5f, 0.5f)]public float ReflectionBlurSpread = 0;
+        [Range(0, 1)]public float LuminanceCloseOpThreshold = 0.5f;
+    }
+    
+    [Reload("Assets/Scripts/HizSSR.compute")]
+    public ComputeShader HizSSRShader; 
+    public SSRComputeFeatureSettings settings = new SSRComputeFeatureSettings();
+    private SSRComputePass m_SSRComputePass;
+
+    private RenderTargetHandle m_ScreenSpaceReflectionTexture;
+    public override void Create()
+    {
+        
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        m_SSRComputePass = new SSRComputePass();
+        m_SSRComputePass.Setup(settings.Event, settings, (UniversalRenderer)renderer, HizSSRShader);
+            
+        renderer.EnqueuePass(m_SSRComputePass);
+    }
+}
---
+++ Assets/Scripts/SSRComputePass.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.XR;
+
+public class SSRComputePass : ScriptableRenderPass
+{
+    private UniversalRenderer m_Renderer;
+    private Material m_Material;
+    private ComputeShader shader;
+    
+    private RenderTexture m_OddBuffer;
+    private RenderTexture m_EvenBuffer;
+    
+    public void Setup(
+        RenderPassEvent renderPassEvent,
+        SSRComputeFeature.SSRComputeFeatureSettings settings,
+        UniversalRenderer renderer, 
+        ComputeShader cs)
+    {
+        this.renderPassEvent = renderPassEvent;
+        m_Renderer = renderer;
+        shader = cs;
+
+        shader.SetFloat("_MaxSteps", settings.MaxSteps);
+        shader.SetFloat("_MaxDistance", settings.MaxDistance);
+        shader.SetFloat("_Thickness", settings.Thickness);
+        shader.SetFloat("_ReflectionStride", settings.ReflectionStride);
+        shader.SetFloat("_ReflectionJitter", settings.ReflectionJitter);
+    }
+
+    public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+    {
+    }
+    
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+    {
+        var cmd = CommandBufferPool.Get("SSRComputePass");
+        
+        cmd.SetComputeTextureParam(shader, 0, "_CameraColorTexture", m_Renderer.cameraColorTarget);
+        cmd.SetComputeTextureParam(shader, 0, "_CameraNormalsTexture", m_Renderer.cameraNormalTarget);
+        cmd.SetComputeTextureParam(shader, 0, "_CameraDepthTexture", m_Renderer.cameraDepthTarget);
+    }
+}
